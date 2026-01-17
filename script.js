69814474220a7da36160a46cb940f30d2124eb77
contasx404 <contasx404@gmail.com>
2026-01-17 22:37:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function getPackage() {
-  alert("Redirecting you to receive your package 🚀");
   setTimeout(() => {
     window.location.href = "https://t.me/GetYourFoldersHere10_14?start=MegaPromo";
   }, 800);
